707cb7c7d5972b369485b41d6a8bf6876913abca
Sukhwinder Singh <31208964+sukha002@users.noreply.github.com>
2024-05-05 23:57:04 -0400

    Add files via upload

--- game.py
+++ game.py
@@ -1,38 +1,25 @@
-
 import random 
   
-
-    
+   
 def declare_winner(Player1, p1, Player2, p2):
     final_score(p1,p2)
     if p1 > p2:
         print(Player1, " is the winner, Your score is .. ")
-        
     elif p1 < p2: 
         print(Player2, " is the winner, Your score is .. ");
-
     else:
         print("So, there is a draw")
-        
-    
-
+     
 def final_score(X,Y):
-    X = X + 1
-    Y = Y + 1
+    X = 1 + X
+    Y = 1 + Y
     print("Score of P1 is ", str(X) + " Score of P2 is ", str(Y))
     return (X,Y)
 
-
-
 def shuffle_letter(word, length):
     shuffled_word = random.sample(word,length)
-    #print(shuffled_word)
+    shuffled_word = ''.join(shuffled_word)
     return shuffled_word
-
-
-
-
-
 
 def choose_rand():
     random_words = [
@@ -62,15 +49,11 @@ def choose_rand():
     return int(valid)
         
 
-
-
-
-
 def play_game():
     Player1 = str(input("Add P1 name : "))
     Player2 = str(input("Add P2 name : "))
-    player_1_score = -1
-    player_2_score = -1
+    player_1_score = 0
+    player_2_score = 0
     while True:
         turn = random.randint(1, 100)
         if turn %2 == 0:
@@ -91,9 +74,6 @@ def play_game():
 
         declare_winner( Player1, player_1_score, Player2, player_2_score)
         return 
-
-
-
 
 # Driver code
 if __name__ == '__main__':
